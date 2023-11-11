300d1e1c841a11ab95b945cabc1974141fa00b9c
Maximiliano Furtado <maximiliano.oliveira.furtado@gmail.com>
2023-11-11 12:12:19 -0300

    chore: go mod init

---
+++ app/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	log.Info().Msgf("log ok")
+
+}
